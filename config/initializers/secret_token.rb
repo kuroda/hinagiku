d4bee4a2858b69e580dc9e70c525ac84ccfb2121
Tsutomu Kuroda <t-kuroda@oiax.jp>
2010-12-29 23:02:04 +0900

    rails new の直後

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nameko::Application.config.secret_token = '576b89401f1e31299a0b768b78be81e90414de1c2135ba681029bf7cfcea62fe52df6c5183a4c59b516736d4e4337dc682c94d30b5e5abb8cd81d0453f269f23'
