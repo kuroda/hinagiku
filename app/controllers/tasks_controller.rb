29f2da0fcedf3da9300c2d15084db7365d9737e7
Tsutomu Kuroda <t-kuroda@oiax.jp>
2010-12-30 23:29:28 +0900

    editアクションの実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,6 +11,10 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
   def create
     @task = Task.create(params[:task])
     redirect_to @task
