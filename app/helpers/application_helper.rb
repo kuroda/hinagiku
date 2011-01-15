0875d2e045b44e150cd612de6955fb661c3a8bc0
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-01-15 17:52:44 +0900

    ヘルパーメソッドによるリファクタリング

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def delete_link(object)
+    link_to "削除", object, :method => :delete,
+      :confirm => "本当に削除しますか？"
+  end
 end
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,13 @@
 module TasksHelper
+  def links_for(task)
+    links = []
+    links << link_to("修正", [ :edit, task ])
+    if task.done?
+      links << link_to("戻す", [ :unfinish, task ], :method => :put)
+    else
+      links << link_to("完了", [ :finish, task ], :method => :put)
+    end
+    links << delete_link(task)
+    raw(links.join(" "))
+  end
 end
