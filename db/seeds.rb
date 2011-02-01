2d9532339eb1ef3e186d472117840d49404f52fe
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-02-01 12:12:01 +0900

    db/seeds.rb 面白い例に変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,19 @@
+names = [
+  "切手を買う",
+  "報告書を書く",
+  "家賃を払う",
+  "猫の餌を買う",
+  "燃えないゴミを出す"
+]
+
 description = "これは説明です。" * 20
-Task.create(:name => "Taks 0", :description => description,
-            :due_date => Date.today, :done => true)
+
+5.times do |n|
+  Task.create(:name => names[n], :description => description,
+    :due_date => n.days.from_now, :done => n.zero?)
+end
 
 1.upto(200) do |n|
   Task.create(:name => "Task #{n}", :description => description,
-              :due_date => (n - 3).days.from_now, :done => false)
+    :due_date => (n - 3).days.from_now, :done => false)
 end
