40215dc1e4afa6bc8f6fb9d63b4848c6f8c59036
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-03-20 00:13:11 +0900

    Task#name, #description 文字数制限

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,5 +5,7 @@ class Task < ActiveRecord::Base
     where([ "name LIKE ?", "%#{query}%" ])
   }
 
-  validates :name, :description, :presence => true
+  validates :name, :presence => true
+  validates :name, :length => { :maximum => 20 }
+  validates :description, :length => { :maximum => 200 }
 end
