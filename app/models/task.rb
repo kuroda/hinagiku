5652b0bb035c4e9288d9ff1cdd34027b92e8aa1b
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-04-13 02:29:01 +0900

    Groupモデル追加; Taskとの関連づけ

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ActiveRecord::Base
+  belongs_to :group
+  
   scope :done, where(:done => true).order("due_date DESC")
   scope :undone, where(:done => false).order("due_date")
   scope :search, lambda { |query|
