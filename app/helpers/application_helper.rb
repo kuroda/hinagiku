99d5c4ce558051b8bd8d520ef1755063bff6974f
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-04-13 02:06:28 +0900

    Revert "ヘルパーメソッドerror_messagesを導入"
    This reverts commit 865817520c07035e4884c298764e5c5033f9c83c.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,15 +5,4 @@ module ApplicationHelper
     link_to "削除", object, :method => :delete,
       :confirm => "本当に削除しますか？"
   end
-
-  def error_messages(object, attribute)
-    full_messages = []
-    object.errors[attribute].each do |message|
-      text = I18n.t("errors.format",
-        :attribute => object.class.human_attribute_name(attribute),
-        :message => message)
-      full_messages << content_tag(:p, text, :class => 'error_message')
-    end
-    full_messages.join().html_safe
-  end
 end
