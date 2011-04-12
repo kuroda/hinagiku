382eb9ca0914ddca6db1eb2a57ec658ed7c38938
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-04-13 02:47:17 +0900

    Revert "Groupモデル追加; Taskとの関連づけ"
    This reverts commit 5652b0bb035c4e9288d9ff1cdd34027b92e8aa1b.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,4 @@
 class Task < ActiveRecord::Base
-  belongs_to :group
-  
   scope :done, where(:done => true).order("due_date DESC")
   scope :undone, where(:done => false).order("due_date")
   scope :search, lambda { |query|
