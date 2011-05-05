f465e3c354180c6b5485f1fcac55ed6cd4c95426
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-05-06 02:24:00 +0900

    Category: nameはuniqueであるべし

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,23 @@
+require 'nkf'
+
 class Category < ActiveRecord::Base
   has_many :tasks, :dependent => :nullify
   
   validates :name, :presence => true, :length => { :maximum => 10 }
+  validates :name, :uniqueness => true
+  
+  before_validation :normalize_values
+  
+  private
+  
+  IDEOGRAPHIC_SPACE = [ 0x3000 ].pack("U")
+  WHITESPACES = "[\s#{IDEOGRAPHIC_SPACE}]"
+  
+  def normalize_values
+    if name.present?
+      self.name = NKF.nkf("-WwZ", name)
+      self.name = name.sub(/^#{WHITESPACES}+/, "")
+      self.name = name.sub(/#{WHITESPACES}+$/, "")
+    end
+  end
 end
