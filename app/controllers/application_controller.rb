47df2b4680cc4e1d96461e5399e2c65af4cc6339
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-05-05 17:02:47 +0900

    例外処理(404, 500)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   clear_helpers
+  
+  rescue_from Exception, :with => :render_500
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
+  private
+  def render_404(exception)
+    raise exception unless Rails.env.production?
+    render "errors/not_found", :status => 404
+  end
+
+  def render_500(exception)
+    raise exception unless Rails.env.production?
+    render "errors/internal_server_error", :status => 500
+  end
 end
