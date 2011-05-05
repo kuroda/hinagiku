939d711798c7083232322870543e819965d70460
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-05-06 04:09:34 +0900

    Task: 参照整合性のチェック

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,4 +9,13 @@ class Task < ActiveRecord::Base
 
   validates :name, :presence => true, :length => { :maximum => 20 }
   validates :description, :length => { :maximum => 200 }
+  validate :check_association
+
+  private
+  def check_association
+    if category_id && !Category.where(:id => category_id).exists?
+      errors.add(:base, :missing_category)
+      self.category_id = nil
+    end
+  end
 end
