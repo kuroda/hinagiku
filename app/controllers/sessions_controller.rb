905a5e45ba0ff3ed33c6f227e43ba5bdaab3254f
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-11-14 02:31:08 +0900

    sessions#create の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,11 @@
 class SessionsController < ApplicationController
+  def create
+    user = User.find_by_login_name(params[:login_name])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to :root
+    else
+      render :new
+    end
+  end
 end
