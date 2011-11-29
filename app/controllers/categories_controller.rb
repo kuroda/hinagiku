5be19dfb3c375d3bdedbf65dbe5d65c518853d5c
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-11-29 22:55:44 +0900

    categoriesコントローラ：アクセス制限

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, :with => :rescue_record_not_found
   
   def index
-    @categories = Category.all
+    @categories = current_user.categories.all
   end
 
   def new
@@ -10,11 +10,12 @@ class CategoriesController < ApplicationController
   end
 
   def edit
-    @category = Category.find(params[:id])
+    @category = current_user.categories.find(params[:id])
   end
 
   def create
     @category = Category.new(params[:category])
+    @category.owner = current_user
     if @category.save
       redirect_to :categories
     else
@@ -23,7 +24,7 @@ class CategoriesController < ApplicationController
   end
 
   def update
-    @category = Category.find(params[:id])
+    @category = current_user.categories.find(params[:id])
     @category.attributes = params[:category]
     if @category.save
       redirect_to :categories
@@ -33,7 +34,7 @@ class CategoriesController < ApplicationController
   end
 
   def destroy
-    @category = Category.find(params[:id])
+    @category = current_user.categories.find(params[:id])
     @category.destroy
     redirect_to :categories
   end
