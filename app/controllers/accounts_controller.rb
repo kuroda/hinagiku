078ce2b5d869c859d81d01534bd8f4b1c3204c42
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-12-09 23:00:42 +0900

    インスタンス変数 @user は不要

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -23,8 +23,7 @@ class AccountsController < ApplicationController
   end
   
   def update
-    @user = current_user
-    if @user.update_attributes(params[:user])
+    if current_user.update_attributes(params[:user])
       redirect_to :account
     else
       render :edit
