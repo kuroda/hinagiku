d17011ce171f43edd29f4729f6198ce77c023839
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-12-11 16:11:36 +0900

    has_secure_password を使用しない

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,29 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
-  has_secure_password
-  
   has_many :tasks, :foreign_key => 'owner_id', :dependent => :destroy
   has_many :categories, :foreign_key => 'owner_id', :dependent => :destroy
   has_many :emails, :dependent => :destroy
   
-  attr_accessible :login_name, :password, :emails_attributes
+  attr_accessor :password
+  attr_accessible :login_name, :password
   
-  accepts_nested_attributes_for :emails
-  
-  validates :login_name, :presence => true, :length => { :maximum => 20 }, :uniqueness => true
+  validates :login_name, :presence => true, :length => { :maximum => 20 },
+    :uniqueness => true
+  validates :password,
+    :length => { :minimum => 4, :allow_nil=> true },
+    :confirmation => true
   
   before_create do
     self.auto_login_token = SecureRandom.hex(32)
+  end
+  
+  before_save do
+    self.password_digest = BCrypt::Password.create(password) if password
+  end
+  
+  def authenticate(unencrypted_password)
+    password_digest &&
+      BCrypt::Password.new(password_digest) == unencrypted_password
   end
 end
