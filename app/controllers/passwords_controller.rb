9526e8b9adec8ebac8acdf9b775b44a08b7aeede
Tsutomu Kuroda <t-kuroda@oiax.jp>
2012-03-25 10:08:34 +0900

    passwordsコントローラ：整形

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,19 +1,13 @@
 class PasswordsController < ApplicationController
   skip_before_filter :reject_unverified_user
 
-  def edit
-  end
-
-def update
-  current_user.attributes = params[:user]
-  current_user.changing_password = true
-  if current_user.save
-    redirect_to :action => :updated
-  else
-    render :edit
-  end
-end
-
-  def updated
+  def update
+    current_user.attributes = params[:user]
+    current_user.changing_password = true
+    if current_user.save
+      redirect_to :action => :updated
+    else
+      render :edit
+    end
   end
 end
